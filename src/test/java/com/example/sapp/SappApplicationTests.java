8631284824b98c07e69785e974ee3cb82ea13e5a
Rahul Bhargav <rahulbhargav26@gmail.com>
2021-10-19 18:00:38 +0530

    1st spring boot

---
+++ src/test/java/com/example/sapp/SappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
